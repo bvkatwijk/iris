4a8e6fd7fb1048b2c1a19c4e24f1c8fc4da5dc4e
Boris van Katwijk <bvkatwijk@gmail.com>
2019-01-30 00:01:22 +0100

    Update ClassDefinitionParserTest.scala

--- src/test/scala/org/bvkatwijk/iris/cd/ClassDefinitionParserTest.scala
+++ src/test/scala/org/bvkatwijk/iris/cd/ClassDefinitionParserTest.scala
@@ -6,32 +6,24 @@ import org.bvkatwijk.iris.id.IdentifierParser.QualifiedIdentifier
 
 class ClassDefinitionParserTest extends ParseTest {
   "classDefinition" - {
-      def classDefinition(value: String) = ClassDefinitionParser(value)
-      def classOf(value: String) = Right(ClassDefinition(QualifiedIdentifier(value)))
+    def classDefinition(value: String) = ClassDefinitionParser(value)
 
-      "type" - {
-        def classType(str: String) = classDefinition(s"class $str {}") should be(classOf(str))
+    def classOf(value: String) = Right(ClassDefinition(QualifiedIdentifier(value)))
 
-        "A" in classType("A")
-        "B" in classType("B")
-        "Type" in classType("Type")
-      }
+    "type" - {
+      def classType(str: String) = classDefinition(s"class $str {}") should be(classOf(str))
+
+      "A" in classType("A")
+      "B" in classType("B")
+      "Type" in classType("Type")
+    }
 
     "with constructor" - {
+      def classConstructorType(str: String) = classDefinition(s"class $str {}")
+
       "empty" - {
-        "A()" in {
-          ClassDefinitionParser {
-            "class A() {}"
-          } should be(Right(ClassDefinition(QualifiedIdentifier("A"))))
-        }
-        "B()" in {
-          ClassDefinitionParser {
-            "class B() {}"
-          } should be(Right(ClassDefinition(QualifiedIdentifier("B"))))
-        }
-      }
-      "with" - {
-        "one parameter" in {}
+        "A()" in { classConstructorType("A()") should be(classOf("A")) }
+        "B()" in { classConstructorType("B()") should be(classOf("B")) }
       }
     }
   }
