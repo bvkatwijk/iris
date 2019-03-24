4bca9ae02b7b0cd3345ddbbfba7336a3726687f5
Boris van Katwijk <bvkatwijk@gmail.com>
2019-03-24 10:34:58 +0100

    Update NameRule.scala

--- src/main/scala/org/bvkatwijk/iris/lang/NameRule.scala
+++ src/main/scala/org/bvkatwijk/iris/lang/NameRule.scala
@@ -3,5 +3,7 @@ package org.bvkatwijk.iris.lang
 import org.parboiled2.{CharPredicate, Parser, Rule1}
 
 trait NameRule { self: Parser =>
-  def name: Rule1[String] = rule { capture(oneOrMore(CharPredicate.LowerAlpha) ~ zeroOrMore(CharPredicate.Alpha)) }
+  def name: Rule1[String] = rule {
+    capture(oneOrMore(CharPredicate.LowerAlpha) ~ zeroOrMore(CharPredicate.Alpha))
+  }
 }
