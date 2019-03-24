60b57afc9c506a854c9941d5b45c3143d893b0ce
Boris van Katwijk <bvkatwijk@gmail.com>
2019-03-24 12:18:30 +0100

    Remove unused

--- src/main/scala/org/bvkatwijk/iris/ast/QualifiedIdentifier.scala
+++ src/main/scala/org/bvkatwijk/iris/ast/QualifiedIdentifier.scala
@@ -2,9 +2,4 @@ package org.bvkatwijk.iris.ast
 
 case class QualifiedIdentifier(pack: Option[Pack] = Option.empty, identifier: Identifier) {
   def javaClassName = identifier.name
-
 }
-object QualifiedIdentifier {
-  def as_identifier(identifier: String): QualifiedIdentifier = QualifiedIdentifier(None, Identifier(identifier))
-}
-
