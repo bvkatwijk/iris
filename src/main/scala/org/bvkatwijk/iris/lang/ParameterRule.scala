ce3d3cd0b360acc1995725f53778a4268452a5f7
Boris van Katwijk <bvkatwijk@gmail.com>
2019-03-24 13:01:30 +0100

    Add parameter rule

---
+++ src/main/scala/org/bvkatwijk/iris/lang/ParameterRule.scala
@@ -0,0 +1,8 @@
+package org.bvkatwijk.iris.lang
+
+import org.bvkatwijk.iris.ast.Parameter
+import org.parboiled2.{Parser, Rule1}
+
+trait ParameterRule { self: Parser with Base with NameRule with QualifiedIdentifierRule =>
+  def parameter: Rule1[Parameter] = rule { name ~ ':' ~ OWS ~ qualifiedIdentifier ~> Parameter }
+}
