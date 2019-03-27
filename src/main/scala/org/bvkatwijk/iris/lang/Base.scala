e9fb32affd5c25a708ca7f4c686f37fa877e05cf
Boris van Katwijk <bvkatwijk@gmail.com>
2019-03-27 19:32:09 +0100

    Update Base.scala

--- src/main/scala/org/bvkatwijk/iris/lang/Base.scala
+++ src/main/scala/org/bvkatwijk/iris/lang/Base.scala
@@ -3,6 +3,15 @@ package org.bvkatwijk.iris.lang
 import org.parboiled2.{Parser, Rule0}
 
 trait Base { self: Parser =>
+  /**
+    * Newlines
+    * @return
+    */
   def NL: Rule0 = rule { optional('\r') ~ '\n' }
+
+  /**
+    * Optional White Space
+    * @return
+    */
   def OWS: Rule0 = rule { zeroOrMore(' ') }
 }
