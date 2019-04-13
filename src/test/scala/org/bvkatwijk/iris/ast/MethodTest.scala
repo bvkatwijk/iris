de5e7fb0b38ff62adf18a05a0d3fc23170f5a8ff
Boris van Katwijk <bvkatwijk@gmail.com>
2019-04-13 12:59:27 +0200

    Update MethodTest.scala

--- src/test/scala/org/bvkatwijk/iris/ast/MethodTest.scala
+++ src/test/scala/org/bvkatwijk/iris/ast/MethodTest.scala
@@ -2,7 +2,7 @@ package org.bvkatwijk.iris.ast
 
 import org.scalatest.{FreeSpec, Matchers}
 
-class MethodTest  extends FreeSpec with Matchers {
+class MethodTest extends FreeSpec with Matchers {
   "Method" - {
     def subject(name: String) = MethodDeclaration(name, Seq(), QualifiedIdentifier(None, Identifier("a")))
     "name" - {
