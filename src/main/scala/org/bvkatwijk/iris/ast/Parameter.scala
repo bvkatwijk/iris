15992e0c522426d992656acaa9ba2f078618423c
Boris van Katwijk <bvkatwijk@gmail.com>
2019-04-13 13:42:49 +0200

    Parameter conversion

--- src/main/scala/org/bvkatwijk/iris/ast/Parameter.scala
+++ src/main/scala/org/bvkatwijk/iris/ast/Parameter.scala
@@ -1,3 +1,9 @@
 package org.bvkatwijk.iris.ast
 
-case class Parameter(name: String, typeValue: QualifiedIdentifier)
+case class Parameter(name: String, typeValue: QualifiedIdentifier) {
+  def asJava: String = typeValue.asFull + " " + name
+}
+
+object Parameter {
+  def unqualified(name: String, identifier: String): Parameter = Parameter(name, QualifiedIdentifier(None, Identifier(identifier)))
+}
--- src/main/scala/org/bvkatwijk/iris/lang/ParameterRule.scala
+++ src/main/scala/org/bvkatwijk/iris/lang/ParameterRule.scala
@@ -1,11 +1,11 @@
 package org.bvkatwijk.iris.lang
 
-import org.bvkatwijk.iris.ast.Parameter
+import org.bvkatwijk.iris.ast.{Parameter, QualifiedIdentifier}
 import org.parboiled2.{Parser, Rule1}
 
 trait ParameterRule { self: Parser with Base with NameRule with QualifiedIdentifierRule =>
   def parameter: Rule1[Parameter] = rule {
-    name ~ ':' ~ OWS ~ qualifiedIdentifier ~> Parameter
+    name ~ ':' ~ OWS ~ qualifiedIdentifier ~> ((name: String, typeValue: QualifiedIdentifier) => Parameter(name, typeValue))
   }
 
   def parameters: Rule1[Seq[Parameter]] = rule {
